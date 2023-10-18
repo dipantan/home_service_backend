211985cb7181e14cc1b896d0527a84c78a4438f2
dipantan <sanju.mahinagar@gmail.com>
2023-10-18 18:00:40 +0530

    asdas

--- auth/register/technician.php
+++ auth/register/technician.php
@@ -56,7 +56,7 @@ if (empty($category) || !array_search($category, $categories->data, true)) {
     // echo "found";
 }
 
-echo $categories;
+print_r($categories);
 
 // echo json_encode($categories);
 
